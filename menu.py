4b8905d5d4bcf7eca5da9b15146071e3defe0382
Cássio Zanon Bindaco <116852683+Ca22io@users.noreply.github.com>
2023-08-09 13:22:31 -0300

    Update menu.py
    Adicionado a classe Menu e seus métodos

--- menu.py
+++ menu.py
@@ -1,32 +1,34 @@
 from funcoes import *
 import os
 
-def menu():
-    os.system('cls')
-    print("\033[1;32m-=-\033[m"*10)
+class Menu():
+    def grid_escolha():
+        os.system('cls')
+        print("\033[1;32m-=-\033[m"*10)
+    
+        print('         \033[4mMenu inicial\033[m')
+        
+        print("\033[1;32m-=-\033[m"*10)
+        
+        print('\033[1;32m-=-\033[m  \033[1m1 - Cadastrar livro\033[m   \033[1;32m-=-\033[m')
+        
+        print('\033[1;32m-=-\033[m  \033[1m2 - Alterar livro\033[m     \033[1;32m-=-\033[m')
+        
+        print('\033[1;32m-=-\033[m  \033[1m3 - Deletar livro\033[m     \033[1;32m-=-\033[m')
+        
+        print('\033[1;32m-=-\033[m  \033[1m4 - Pesquisar livro\033[m   \033[1;32m-=-\033[m')
+        
+        print('\033[1;32m-=-\033[m  \033[1m5 - Listar livros\033[m     \033[1;32m-=-\033[m')
+        
+        print('\033[1;32m-=-\033[m  \033[1m6 - Sair\033[m              \033[1;32m-=-\033[m')
+        
+        print("\033[1;32m-=-\033[m"*10)
+        
+        return int(input('Escolha uma opção: '))
 
-    print('         \033[4mMenu inicial\033[m')
-    
-    print("\033[1;32m-=-\033[m"*10)
-    
-    print('\033[1;32m-=-\033[m  \033[1m1 - Cadastrar livro\033[m   \033[1;32m-=-\033[m')
-    
-    print('\033[1;32m-=-\033[m  \033[1m2 - Alterar livro\033[m     \033[1;32m-=-\033[m')
-    
-    print('\033[1;32m-=-\033[m  \033[1m3 - Deletar livro\033[m     \033[1;32m-=-\033[m')
-    
-    print('\033[1;32m-=-\033[m  \033[1m4 - Pesquisar livro\033[m   \033[1;32m-=-\033[m')
-    
-    print('\033[1;32m-=-\033[m  \033[1m5 - Listar livros\033[m     \033[1;32m-=-\033[m')
-    
-    print('\033[1;32m-=-\033[m  \033[1m6 - Sair\033[m              \033[1;32m-=-\033[m')
-    
-    print("\033[1;32m-=-\033[m"*10)
-    
-    return int(input('Escolha uma opção: '))
-
+menu = Menu()
 while True:
-    escolha = menu()
+    escolha = menu(grid_escolha)
 
     if escolha == 1:
          cadastrar_livr()
