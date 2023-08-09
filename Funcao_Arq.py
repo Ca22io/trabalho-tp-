7864a375e7e63fbffc54027f177f1ec582e245ef
Cássio Zanon Bindaco <116852683+Ca22io@users.noreply.github.com>
2023-08-09 09:22:34 -0300

    Update Funcao_Arq.py
    Adicionando os métodos para a classe arquivo

--- Funcao_Arq.py
+++ Funcao_Arq.py
@@ -3,30 +3,30 @@ import os.path
 
 nome_arquivo = 'livros.json'
 
-
-def ler_arquivo():
-    if not os.path.isfile(nome_arquivo):
-        criarArquivo()
-
-    arq = open(nome_arquivo, 'r', encoding='utf-8')
-    data = arq.read()
-
-    if len(data) == 0:
-        return []
-
-    data = json.loads(data)
-    arq.close()
-
-    return data
-
-
-def salvarArquivo(livros: list):
-    arq = open('livros.json', 'w+', encoding='utf-8')
-    data = json.dumps(livros, indent=5)
-    arq.write(data)
-    arq.close()
-
-
-def criarArquivo():
-    arq = open(nome_arquivo, 'w+', encoding='utf-8')
-    arq.close()
+class Arquivo():
+    def ler_arquivo():
+        if not os.path.isfile(nome_arquivo):
+            criarArquivo()
+    
+        arq = open(nome_arquivo, 'r', encoding='utf-8')
+        data = arq.read()
+    
+        if len(data) == 0:
+            return []
+    
+        data = json.loads(data)
+        arq.close()
+    
+        return data
+    
+    
+    def salvarArquivo(livros: list):
+        arq = open('livros.json', 'w+', encoding='utf-8')
+        data = json.dumps(livros, indent=5)
+        arq.write(data)
+        arq.close()
+    
+    
+    def criarArquivo():
+        arq = open(nome_arquivo, 'w+', encoding='utf-8')
+        arq.close()
